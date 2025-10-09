beae6981be5580c49b8d62c00b29eb7a328c394c
mashu2315 <mauryaashutosh1983@gmail.com>
2025-10-09 23:00:05 +0530

    CORS Issue resolve

--- server/index.js
+++ server/index.js
@@ -22,7 +22,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(morgan('dev'));
 
